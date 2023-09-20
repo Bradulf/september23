5f24ddf58cf74820bc8df11d2ba91ed5ee3e7d07
Brad <Bradkharris@gmail.com>
2023-09-20 15:35:50 -0700

    first commit

---
+++ src/main/java/dev/lordbrad/september/SeptermberApplication.java
@@ -0,0 +1,13 @@
+package dev.lordbrad.september;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeptermberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeptermberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/lordbrad/september/SeptermberApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lordbrad.september;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeptermberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
