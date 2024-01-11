3dde708b5f46823b1cf5a9f73fb623441505fbd7
Brad <Bradkharris@gmail.com>
2024-01-11 15:31:41 -0700

    1st

---
+++ two/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Brad" + " " + "Harris")
+	fmt.Println("1 + 1 = ", 1+1)
+	var s string
+	s = "brad"
+	fmt.Println(s)
+	f := "apple"
+	fmt.Println(f)
+}
